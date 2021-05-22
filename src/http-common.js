69934c42fb5f0ca607c1e2ec50d0f77600cb1fb7
Akash Singh <Aka080@users.noreply.github.com>
2021-05-22 20:27:43 +0530

    json server link

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: " https://my-json-server.typicode.com/Aka080/online-tuts-app",
+  baseURL: "https://my-json-server.typicode.com/Aka080/online-tuts-app",
   headers: {
     "Content-type": "application/json"
   }
