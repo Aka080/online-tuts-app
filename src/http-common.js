825cca4ac7dba03b56eedc0ef2d1de4dbceeea46
Akash Singh <Aka080@users.noreply.github.com>
2021-05-22 20:25:58 +0530

    removing yarn lock

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: " https://my-json-server.typicode.com/Aka080/online-tuts-app",
   headers: {
     "Content-type": "application/json"
   }
